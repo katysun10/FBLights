487ec7b0526bd206a8663f8e286e3a209d87b54a
asunny72 <andydongsun@gmail.com>
2017-05-17 20:57:06 -0400

    stub out some functions for pseudo coding

--- clean.js
+++ clean.js
@@ -1,4 +1,4 @@
- const SEARCH_INTERVAL = 1000;
+const SEARCH_INTERVAL = 1000;
 
 const USER_CONTENT_ELEMENT = ".userContentWrapper, .fbUserContent";
 
@@ -11,22 +11,55 @@ const HIGHLIGHT_KEYWORDS = [
     "thanks"
 ];
 
-function filter(node) {
-    
+/*
+ * @method
+ * @param {Object} post a reference to the DOM element that renders a fb post
+ * @param {String} color the color to highlight the post with
+ */
+function highlight (post, color) {
+
 }
 
-function cleanNewsFeed() {
+/*
+ * @method
+ * @param {Object} post a reference to the DOM element that renders a fb post
+ * @returns {Boolean} returns true if keyword is found, false otherwise
+ */
+function containsKeyword (post) {
+
+}
+
+/*
+ * @method
+ * @param {Object} post a reference to the DOM element that renders a fb post
+ * @returns {Boolean} returns true if link is found, false otherwise
+ */
+function containsLink (post) {
+
+}
+
+/*
+ * @method
+ * Extracts references to fb posts and searches post for links or keywords for
+ * highlighting
+ */
+function highlightNewsFeed() {
     var posts = document.querySelectorAll(USER_CONTENT_ELEMENT);
-   //posts.forEach(filter);
-   //console.log(posts);
-   //console.log(posts.length);
-   console.log(posts);
-    for(var i = 0; i < posts.length; i++){
-        filter(posts[i]);
-        //console.log(posts[i]);
+    for (var i = 0; i < posts.length; i++) {
+        // TODO
+        // Assume that containsLink, containsKeyword, and highlight are
+        // fully functional. 
+        // In pseudo code, use these building blocks to pseduo code out
+        // how you would implement the highlight logic.
+        // For now, if there is something you want to do, make a function
+        // for it.
     }
 }
 
+/*
+ * Entry Point
+ * Continuously loop and highlight the news feed
+ */
 setInterval(function () {
         // Don't do anything if the FB UI hasn't loaded yet
         var streamContainer = document.querySelector("[id*='main_stream']");
@@ -34,6 +67,6 @@ setInterval(function () {
             return;
         }
 
-        cleanNewsFeed();
+        highlightNewsFeed();
 
 }, SEARCH_INTERVAL);
